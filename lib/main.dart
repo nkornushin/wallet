0d9c4be9b628c3591ccc28251cb4d0489d82dc4b
nkornushin <nkornushin@gmail.com>
2020-04-25 15:53:53 +0300

    Update main.dart
    Remove not needed comment

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:wallet/screens/transaction_report.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
